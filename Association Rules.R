3f313a4057c274381d025b33b637481448d1369a
Neha2823 <64286770+Neha2823@users.noreply.github.com>
2020-04-27 08:40:56 +0530

    Add files via upload

---
+++ Association Rules.R
@@ -0,0 +1,17 @@
+# Association Rules
+install.packages("arules")
+library(arules)
+Titanic<-read.csv("c:/users/NEHA_BGL/desktop/Titanic.csv")
+
+Titanic<-Titanic[,-c(1)]
+rules <- apriori(Titanic)
+arules::inspect(rules)
+rules.sorted <- sort(rules, by="lift")
+arules::inspect(rules.sorted)
+
+# rules with rhs containing "Survived" only
+rules <- apriori(Titanic,parameter = list(supp=0.1, conf=0.5)
+                 ,appearance = list(rhs=c("Survived=No", "Survived=Yes")
+                 ),control = list(verbose=F))
+arules::inspect(rules)
+
