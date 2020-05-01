6c68b508d39775692be0bad5910f720393da4a96
Neha2823 <64286770+Neha2823@users.noreply.github.com>
2020-05-01 10:39:22 +0530

    Add files via upload

---
+++ Linear Regression.R
@@ -0,0 +1,28 @@
+#Load data
+
+Nd<-read.csv("c:/users/NEHA_BGL/desktop/NewspaperData.csv")
+
+# Visualization
+install.packages("lattice")
+install.packages("ggplot2")
+library(lattice)
+dotplot(Nd$sunday, main="Dot Plot of Sunday Circulations",col="dodgerblue4")
+dotplot(Nd$daily, main="Dot Plot of Daily Circulations", col="dodgerblue4")
+boxplot(Nd$sunday,col="dodgerblue4")
+boxplot(Nd$daily,col="dodgerblue4")
+
+#Regression equation
+#Syntax  model<-lm(y~x,data=data set name)
+colnames(Nd)
+model<- lm(sunday~daily,data =Nd)
+summary(model)
+new_daily=data.frame(daily=c(200,300))
+sun1=predict(model,newdata=new_daily)
+sun1
+
+pred<-predict(model)
+pred
+
+pred<-predict(model)
+finaldata<-data.frame(Nd,pred,"Error" = Nd$sunday-pred)
+View(finaldata)
