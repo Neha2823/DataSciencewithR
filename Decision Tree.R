9ee40e043fb8e6a5084e3958b6ec865923707231
Neha2823 <64286770+Neha2823@users.noreply.github.com>
2020-05-07 07:36:27 +0530

    Add files via upload

---
+++ Decision Tree.R
@@ -0,0 +1,20 @@
+# Load the libraries
+
+library(caret)
+library(C50)
+data("iris")
+set.seed(7)
+inTraininglocal<-createDataPartition(iris$Species,p=.70,list = F)
+training<-iris[inTraininglocal,]
+testing<-iris[-inTraininglocal,]
+
+#Model Building
+model<-C5.0(Species~.,data = training) 
+#Generate the model summary
+summary(model)
+#Predict for test data set
+pred<-predict.C5.0(model,testing[,-5])
+a<-table(testing$Species,pred)
+sum(diag(a))/sum(a)
+plot(model)
+
