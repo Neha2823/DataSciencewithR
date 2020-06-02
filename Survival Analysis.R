b8f4c3bf4dcf872cf04e74413da9e408f5f77a0d
Neha2823 <64286770+Neha2823@users.noreply.github.com>
2020-06-02 08:01:14 +0530

    Add files via upload

---
+++ Survival Analysis.R
@@ -0,0 +1,25 @@
+install.packages("survival")
+library(survival)
+survival_unemployment1<-read.csv("C:/Users/Neha_BGL/Desktop/survival_unemployment1.csv")
+
+attach(survival_unemployment1)
+
+# Define variables 
+time <- spell
+group <- ui
+X <- cbind(logwage, ui, age)
+# Descriptive statistics
+summary(time)
+summary(event)
+summary(X)
+summary(group)
+
+# Kaplan-Meier non-parametric analysis
+kmsurvival <- survfit(Surv(time,event) ~ 1)
+
+plot(kmsurvival, xlab="Time", ylab="Survival Probability")
+# Kaplan-Meier non-parametric analysis by group
+kmsurvival1 <- survfit(Surv(time, event) ~ group)
+
+plot(kmsurvival1,xlab="Time", ylab="Survival Probability",col = c(2,3),conf.int = T)
+
